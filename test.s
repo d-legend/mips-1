77449732597f2c1d3bc1f3e8f5dba277754c34d5
d-legend <jon.jd@hotmail.com>
2017-11-13 14:21:54 -0500

    searching buffer

--- test.s
+++ test.s
@@ -4,17 +4,41 @@ string_ask: .asciiz "\nPlease enter your string: "
 
 string_return: .asciiz "\nResult: "
 
-in_name: .space 31 ## space for input string
+in_name: .space 8 ## space for input string
 
 
 .text
 
-.global main
+.globl main
 
 main:
 
-la $a0,string_ask ## print prompt string
-
-li $v0,4
+  la $a0,string_ask ## print prompt string
+  li $v0,4
 
 syscall
+
+  la $a0,in_name ## read the input string
+  li $a1,8 ## at most 30 chars + 1 null char
+  li $v0,8
+
+syscall
+xor $a0, $a0, $a0
+search:  
+  lbu $a2, in_name($a0)
+  beq $a2, 'a', found    
+  addiu $a0, $a0, 1
+  bne $a0, $a1, search
+
+
+not_found:
+    # Code here is executed if the character is not found
+    b done
+found:
+	addi $s7, $s7,239
+    # Code here is executed if the character is found
+done:
+
+
+
+
