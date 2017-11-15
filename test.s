ee6dca9fcf14269db41e8959ac9515737391efc7
d-legend <jon.jd@hotmail.com>
2017-11-15 04:12:36 -0500

    Fixed white space filtering
    Correctly accepts defined input values and Ignores white space where applicable

--- test.s
+++ test.s
@@ -8,7 +8,7 @@ string_done: .asciiz "\nDone."
 
 string_invalid: .asciiz "\nInvalid hexadecimal number."
 
-buff: .space 8 # space for input string
+buff: .space 9 # space for input string
 
 
 .text
@@ -32,9 +32,9 @@ strLen:                 #getting length of string
 
 lbu $t4, buff($a0)   #loading value
 beq $t4, 10, HextoDec
-beq $t4, $zero, HextoDec
+beq $t4, 0, HextoDec
 add $a0, $a0, 1
-add $s5, $s5, 1  
+add $s6, $s6, 1  
 
 #
 #Checking the character to see if it is in range
@@ -64,12 +64,16 @@ and  $t0, $t1, $t0    # Sets $t0=1 if $a1 < $a0 < $a2, otherwise $t0=0
 or $t2, $t2, $t0    
 
 
+WhiteSpaces:
+move $a3, $a0
+move $t7, $t4
 li $t6,32
-bne $t6,$t4,Else   # b Else if there is no space
+bne $t6,$t7,Else   # b Else if there is no space
 beq $s5, 8, Invalid # b Invalid if all spaces
-#beq $t8, 1, 
+beq $t8, 1, LoopBuff 
 li $t2, 1          # $t2 = 1
 j Exit             # jump out of the if
+
 Else: 
 	bne $t2,1,Invalid # b Invalid if other chars not hex
 	li $t8, 1 	#Assign 1 if you see a char
@@ -80,16 +84,36 @@ beq $t2, $zero, Invalid #branch to invalid if the character is not valid
 j strLen
 
 #
+#After there is a character and then a space iterate further to #see if another character shows up and mark invalid if it is so
+#
+
+LoopBuff: 
+	
+lbu $t7, buff($a3)   #loading value
+add $a3, $a3, 1
+bne $t7, 32, Else2
+j LoopBuff
+	Else2:
+	bne $t7, 10, Else3
+		li $t8, 0 
+		j WhiteSpaces
+Else3:
+		bne $t7, 0, Invalid
+		li $t8, 0 
+		j WhiteSpaces
+
+#
 #Hexadecimal to Decimal Conversion
 #
 
 HextoDec:
-	blez $s5, Invalid  # jump Invalid on hitting enter first
+	blez $s6, Invalid  # jump Invalid on hitting enter first
 la $a0,string_done # end of prog
 li $v0,4
 syscall
 li $v0,10
 syscall
+
 
 
 done:
